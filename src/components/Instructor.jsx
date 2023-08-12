b25c481a6c7adc84e2c648ce43943519bdb3b154
saloniprasad9 <saloniprasad9@gmail.com>
2023-08-12 19:29:45 +0530

    changes in instructor component

--- src/components/Instructor.jsx
+++ src/components/Instructor.jsx
@@ -10,42 +10,43 @@ const Instructor = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
         <h2 className={`${styles.heading2}`}>Instructed by</h2>
-        <div className='flex flex-row rounded-[8px] bg-green_gradient2 h-[800px] w-[800px] px-4'>
-            <img src={vaibhav} alt='vaibhav' className='w-[300px] h-[500px]'/>
-            <div className='flex flex-col'>
+        <div className='flex flex-row rounded-[8px] space-x-8 border-white border-[1px] bg-green_gradient2 h-[720px] w-[1000px] px-8 py-8'>
+            <img src={vaibhav} alt='vaibhav' className='w-[500px] h-[600px] rounded-[10px]'/>
+            <div className='flex flex-col space-y-4'>
                 <h2 className={`${styles.heading2}`}>Vaibhav Sisinty</h2>
-                <h2 className={`${styles.heading2}`}>Growth Hacker, Entrepreneur Ex. Uber, Klook</h2>
-                <p className='text-white'>I turn startups into profitable businesses, growth consult for brands & help them use the internet to increase profitability & their overall bottom lines.</p>
-                <p className='text-white'>In 2012 (at age 19), I moved from running Discovery Android to founding an award winning digital media company, CrazyHeads. Within 3 years, CrazyHead helped 100+ clients over the world grow their businesses using internet.</p>
-                <p className='text-white'>After working at Uber ( India & Latin America ) for 4.5 years, I joined Klook to lead marketing for India & the Middle East where I scaled the business to over $27M in a year.</p>
-                <div className='flex flex-row'>
-                    <div className='bg-[#3e4503] justify-center items-center'>
+                <h2 className={` text-white font-dm`}>Growth Hacker, Entrepreneur Ex. Uber, Klook</h2>
+                <p className='text-white font-dm'>I turn startups into profitable businesses, growth consult for brands & help them use the internet to increase profitability & their overall bottom lines.</p>
+                <p className='text-white font-dm'>In 2012 (at age 19), I moved from running Discovery Android to founding an award winning digital media company, CrazyHeads. Within 3 years, CrazyHead helped 100+ clients over the world grow their businesses using internet.</p>
+                <p className='text-white font-dm'>After working at Uber ( India & Latin America ) for 4.5 years, I joined Klook to lead marketing for India & the Middle East where I scaled the business to over $27M in a year.</p>
+                <div className='flex flex-row space-x-4 bg-[#3e4503] w-[400px] rounded-[8px]'>
+                    <div className='bg-[#3e4503] flex justify-center w-[200px] items-center rounded-[8px]'>
                         <img src={linkedin} alt='linkedin' className='h-[30px] w-[30px]'/>
                     </div>
-                    <div className='bg-[#232602] flex flex-row'>
-                        <h2 className='text-white'>150M+</h2>
-                        <p className='text-white'>impressions</p>
-                        <h2 className='text-white'>330K+</h2>
-                        <p className='text-white'>followers </p>
+                    <div className='bg-[#232602] flex flex- space-x-4 justify-center items-center px-2 rounded-[8px]'>
+                        <h2 className='text-white font-dm'>150M+</h2>
+                        <p className='text-white font-dm'>impressions</p>
+                        <div className='bg-[white] w-[1px] h-[15px]'></div>
+                        <h2 className='text-white font-dm'>330K+</h2>
+                        <p className='text-white font-dm'>followers </p>
                     </div>
                 </div>
-                <div className='flex flex-row'>
-                    <div className='flex flex-row'>
-                        <div className='bg-[#3e4503]'>
+                <div className='flex flex-row space-x-4'>
+                    <div className='flex flex-row rounded-[8px]'>
+                        <div className='bg-[#3e4503] rounded-[8px]'>
                             <img src={instagram} alt='instagram'/>
                         </div>
-                        <div className='bg-[#232602] flex flex-row'>
-                            <h2 className='text-white'>84K+</h2>
-                            <p className='text-white'>followers </p>
+                        <div className='bg-[#232602] rounded-[8px] flex flex-row items-center space-x-4 justify-center'>
+                            <h2 className='text-white font-dm'>84K+</h2>
+                            <p className='text-white font-dm'>followers </p>
                         </div>
                     </div>
                     <div className='flex flex-row'>
                         <div className='bg-[#3e4503]'>
                             <img src={twitter} alt='twitter'/>
                         </div>
-                        <div className='bg-[#232602] flex flex-row'>
-                            <h2 className='text-white'>75K+</h2>
-                            <p className='text-white'>followers </p>
+                        <div className='bg-[#232602] flex flex-row items-center rounded-[8px] space-x-4 justify-center'>
+                            <h2 className='text-white font-dm'>75K+</h2>
+                            <p className='text-white font-dm'>followers </p>
                         </div>
                     </div>
                 </div>
